e78eb116200b263696b8f7fe48c56a36069bdceb
Anibal Gomez <anibalgomez@balerocms.com>
2015-07-05 12:40:22 -0500

    Newsletter Link

---
+++ src/main/java/com/neblina/balero/web/authorized/NewsletterController.java
@@ -0,0 +1,31 @@
+/**
+ * Balero CMS v2 Project: Proyecto 100% Mexicano de código libre.
+ *
+ * @author      Anibal Gomez <anibalgomez@icloud.com>
+ * @copyright   Copyright (C) 2015 Neblina Software. Derechos reservados.
+ * @license     Licencia Pública GNU versión 3 o superior; vea LICENSE.txt
+ */
+
+package com.neblina.balero.web.authorized;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/newsletter")
+public class NewsletterController {
+
+    private static final Logger log = LogManager.getLogger(NewsletterController.class.getName());
+
+
+    @Secured("ROLE_ADMIN")
+    @RequestMapping(value = {"", "/"} )
+    public String homepage(Model model) {
+        return "authorized/newsletter";
+    }
+
+}
