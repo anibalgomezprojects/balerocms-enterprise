8192ba0b47ae4e5226a1a869687cea9aa95a7f64
Anibal Gomez <anibalgomez@balerocms.com>
2015-07-15 11:31:42 -0500

    Optimization SQL Code

--- src/main/resources/db/migration/V-20151406.112400__Init_setup_and_sample_data.sql
+++ src/main/resources/db/migration/V-20151406.112400__Init_setup_and_sample_data.sql
@@ -41,16 +41,19 @@ CREATE TABLE BLOCK (
 	code varchar(255) not null
 );
 
-insert into USER (username, password, password_verify, first_name, last_name, roles) values ('admin', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', 'Anibal', 'Gomez', 'ROLE_ADMIN');
-insert into USER (username, password, password_verify, first_name, last_name, roles) values ('user', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', 'Jon', 'Doe', 'ROLE_USER');
-insert into USER (username, password, password_verify, first_name, last_name, roles) values ('anonymous', '$$$$$$$$', '$$$$$$$$', 'Anonymous', 'Unregistered', 'ROLE_ANONYMOUS');
+INSERT INTO USER (id, username, password, password_verify, first_name, last_name, roles) VALUES
+(1, 'admin', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', 'Anibal', 'Gomez', 'ROLE_ADMIN'),
+(2, 'user', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', 'Jon', 'Doe', 'ROLE_USER'),
+(3, 'anonymous', '$$$$$$$$', '$$$$$$$$', 'Anonymous', 'Unregistered', 'ROLE_ANONYMOUS');
 
-insert into MAIL (email_id, address, user_id) values (1, 'admin@localhost.com', 1);
-insert into MAIL (email_id, address, user_id) values (2, 'user@localhost.com', 2);
-insert into MAIL (email_id, address, user_id) values (3, 'anonymous@localhost.com', 3);
-insert into MAIL (email_id, address, user_id) values (4, 'guest@localhost.com', 3);
+INSERT INTO MAIL (email_id, address, user_id) VALUES
+(1, 'admin@localhost.com', 1),
+(2, 'user@localhost.com', 2),
+(3, 'anonymous@localhost.com', 3),
+(4, 'guest@localhost.com', 3);
 
-insert into SETTING (code, title, title_header, administrator_email, tags, footer, offline, offline_message) values ('en_US', 'Balerocms v2', '<h1>Welcome</h1><h3>Example Portal</h3><hr class="intro-divider" /><p>Congratulations! Installation success!</p>', 'admin@localhost', 'Business, Enterprise, Company, Etc...', '<ul class="list-inline"><li><a href="#home">Home</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#about">About</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#services">Services</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#contact">Contact</a></li></ul><br />(c) 2015. Your company. Powered by <a href="http://www.balerocms.com/">BaleroCMS</a>.', '0', 'Site under maintenance. We will back shortly.');
+INSERT INTO SETTING (id, code, title, title_header, administrator_email, tags, footer, offline, offline_message) VALUES
+(1, 'en_US', 'Balerocms v2', '<h1>Welcome</h1><h3>Example Portal</h3><hr class="intro-divider" /><p>Congratulations! Installation success!</p>', 'admin@localhost', 'Business, Enterprise, Company, Etc...', '<ul class="list-inline"><li><a href="#home">Home</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#about">About</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#services">Services</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#contact">Contact</a></li></ul><br />(c) 2015. Your company. Powered by <a href="http://www.balerocms.com/">BaleroCMS</a>.', '0', 'Site under maintenance. We will back shortly.');
 
 INSERT INTO BLOCK (id, name, content, code) VALUES
 (1, 'tablet_en', '<div class="content-section-a">    <div class="container">        <div class="row">            <div class="col-lg-5 col-sm-6"><hr class="section-heading-spacer">                <div class="clearfix"></div>                <h2 class="section-heading">Enterprise CMS</h2>                <p class="lead">Made with the powerful of Java 8, Hibernate And Spring. You can edit                 this content from your <a href="/admin">Admin</a> Dashboard Panel. You can optimize                this website from your tablet.</p>            </div>            <div class="col-lg-5 col-lg-offset-2 col-sm-6">                <img class="img-responsive" src="/images/ipad.png" alt="" />            </div>        </div>    </div></div>', 'en_US'),
