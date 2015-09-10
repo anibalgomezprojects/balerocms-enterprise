74d8f9a3c1aa365ff0605b239ea9d063f936d06b
Anibal Gomez <anibalgomez@balerocms.com>
2015-09-10 10:47:43 -0500

    Removed

--- src/main/resources/static/scripts/app/app.js
+++
@@ -1,5 +0,0 @@
-angular.module('myApp', [])
-.controller('customersCtrl', function($scope, $http) {
-    $http.get("../test/info.json")
-        .success(function(response) {$scope.names = response.records;});
-});
