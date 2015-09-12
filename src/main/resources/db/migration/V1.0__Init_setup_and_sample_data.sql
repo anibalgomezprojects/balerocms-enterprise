4bba6b13cd37c1a0f1eb545d76886a0930c6bdd4
Anibal Gomez <anibalgomez@balerocms.com>
2015-09-12 16:23:52 -0500

    H2DB/MYSQL Boolean

--- src/main/resources/db/migration/V1.0__Init_setup_and_sample_data.sql
+++ src/main/resources/db/migration/V1.0__Init_setup_and_sample_data.sql
@@ -6,14 +6,14 @@ CREATE TABLE IF NOT EXISTS USER (
 	first_name varchar(255) not null,
 	last_name varchar(255) not null,
 	email varchar(255) not null,
-	subscribed bit not null,
+	subscribed bit(1) not null,
 	roles varchar(255) not null
 );
 
 CREATE TABLE IF NOT EXISTS PROPERTY (
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	administrator_email varchar(255) not null,
-	offline bit not null
+	offline bit(1) not null
 );
 
 CREATE TABLE IF NOT EXISTS SETTING (
@@ -66,12 +66,12 @@ CREATE TABLE IF NOT EXISTS BLOG (
 );
 
 INSERT INTO USER (id, username, password, password_verify, first_name, last_name, email, subscribed, roles) VALUES
-(1, 'admin', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', 'Anibal', 'Gomez', 'anibalgomez@balerocms.com', 'true', 'ROLE_ADMIN'),
-(2, 'user', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', 'Jon', 'Doe', 'demo@localhost.com', 'true', 'ROLE_USER'),
-(3, 'anonymous', '$$$$$$$$', '$$$$$$$$', 'Anonymous', 'Unregistered', 'anonymous@localhost.com', 'true', 'ROLE_ANONYMOUS');
+(1, 'admin', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', '$2a$10$hdOPxpQhV7sEHoSCZk9pBuQkEUYB0AWk.1DZlNgVwxe.CStQNltxm', 'Anibal', 'Gomez', 'anibalgomez@balerocms.com', 1, 'ROLE_ADMIN'),
+(2, 'user', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', '$2a$10$OhggAS1e4GiznN2QrPTHn.V1/FK4QkobOmqHFUPPA4inZcCSoqXKu', 'Jon', 'Doe', 'demo@localhost.com', 1, 'ROLE_USER'),
+(3, 'anonymous', '$$$$$$$$', '$$$$$$$$', 'Anonymous', 'Unregistered', 'anonymous@localhost.com', 1, 'ROLE_ANONYMOUS');
 
 INSERT INTO PROPERTY (id, administrator_email, offline) VALUES
-(1, 'admin@localhost', 'false');
+(1, 'admin@localhost', 0);
 
 INSERT INTO SETTING (id, code, title, title_header, tags, footer, offline_message) VALUES
 (1, 'en', 'Demo', '<h1>Welcome</h1><h3>Example Portal</h3><hr class="intro-divider" /><p>Congratulations! Installation success!</p>', 'Business, Enterprise, Company, Etc...', '<ul class="list-inline"><li><a href="#home">Home</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#about">About</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#services">Services</a></li><li class="footer-menu-divider">&sdot;</li><li><a href="#contact">Contact</a></li></ul><br />(c) 2015. Your company. Powered by <a href="http://www.balerocms.com/">BaleroCMS</a>.', 'Site under maintenance. We will back shortly.'),
